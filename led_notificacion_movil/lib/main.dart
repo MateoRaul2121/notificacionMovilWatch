5110b0e79657b347d127a7b1e19ec8d707b33dc5
MateoRaul2121 <126632138+MateoRaul2121@users.noreply.github.com>
2024-07-08 12:35:52 -0600

    ActualizacionFinalMovil

--- led_notificacion_movil/lib/main.dart
+++ led_notificacion_movil/lib/main.dart
@@ -1,55 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:led_notificacion_movil/pages/bluetooth_page.dart';
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
+@pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  _showNotification(message.notification!.title, message.notification!.body);
-}
 
-void main() async {
+  print("Handling a background message: ${message.messageId}");
+}bool userauth = false;
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp( );
+  requestNotificationPermission();
+  
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
-
-  final InitializationSettings initializationSettings =
-      InitializationSettings(android: initializationSettingsAndroid);
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-
-  runApp(MyApp());
-}
-
-void _showNotification(String? title, String? body) async {
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-      AndroidNotificationDetails(
-    'high_importance_channel', 'High Importance Notifications',
-    importance: Importance.max,
-    priority: Priority.high,
-    showWhen: false,
-  );
-  const NotificationDetails platformChannelSpecifics =
-      NotificationDetails(android: androidPlatformChannelSpecifics);
-  await flutterLocalNotificationsPlugin.show(
-      0, title, body, platformChannelSpecifics,
-      payload: 'item x');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.notification?.title}');
+    
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  FirebaseAuth.instance
+  .authStateChanges()
+  .listen((User? user) {
+    if (user == null) {
+     userauth = false;
+    } else {
+      userauth = true;
+    }
+  });
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('Message data: $fcmToken');
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BluetoothScreen(),
-      debugShowCheckedModeBanner: false, // Ocultar el banner de debug
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
+  }
+}
+
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('Notification permission granted: ${settings.authorizationStatus}');
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BluetoothScreen(),
+    );
+  }
+}
+
+Future<void> _sendNotification(String message) async {
+  final databaseReference = FirebaseDatabase.instance.ref();
+  try {
+    await databaseReference.child('notifications').push().set({'message': message});
+    print('Notification sent successfully: $message');
+  } catch (error) {
+    print('Failed to send notification: $error');
   }
 }
--- led_notificacion_movil/lib/pages/apagado.dart
+++ led_notificacion_movil/lib/pages/apagado.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'white.dart'; // Importar la pantalla WhitePage
 import 'dart:typed_data';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -21,13 +29,96 @@ class PantallaApagado extends StatefulWidget {
   State<PantallaApagado> createState() => _PantallaApagadoState();
 }
 
+Future<String> getAccessToken() async {
+  // Your client ID and client secret obtained from Google Cloud Console
+  final serviceAccountJson = {
+    //CredencialesPropias
+};
+
+ List<String> scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/firebase.database",
+ "https://www.googleapis.com/auth/firebase.messaging"
+];
+ 
+ http.Client client = await auth.clientViaServiceAccount(
+    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+    scopes,
+  );
+
+  // Obtain the access token
+  auth.AccessCredentials credentials = await auth.obtainAccessCredentialsViaServiceAccount(
+    auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+    scopes,
+    client
+  );
+
+  // Close the HTTP client
+  client.close();
+
+  // Return the access token
+  return credentials.accessToken.data;
+
+}
+
+Future<void> sendFCMMessage() async {
+  final String serverKey = await getAccessToken() ; // Your FCM server key
+  final String fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/lednotificaciones/messages:send';
+  final  currentFCMToken = await FirebaseMessaging.instance.getToken();
+  final String tokenRelo = 'cb0yyPokT82HkagKjWyElR:APA91bG7DCFfgcR7DTtv97NxwfI7HduoFbAHoB1KLL8FUPJml5k4pQ4VtzTsGanyhEE7-Xz-7RQc738ppm7gNhFFXdL7-p3evhUDwuxbBMh0_sUHVS1W3eUjRC-pj6MO0bVBcDQyDIvd';
+  print("fcmkey : $currentFCMToken");
+  final Map<String, dynamic> message = {
+    'message': {
+      'token': tokenRelo, // Token of the device you want to send the message to
+      'notification': {
+        'body': 'LED Encendido',
+        'title': 'Cambio de LED'
+      },
+      'data': {
+        'current_user_fcm_token': currentFCMToken, // Include the current user's FCM token in data payload
+      },
+    }
+  };
+
+  final http.Response response = await http.post(
+    Uri.parse(fcmEndpoint),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $serverKey',
+    },
+    body: jsonEncode(message),
+  );
+
+  if (response.statusCode == 200) {
+    print('FCM message sent successfully');
+  } else {
+    print('Failed to send FCM message: ${response.statusCode}');
+  }
+}
+
+Future<void> _sendNotification(String message) async {
+  final databaseReference = FirebaseDatabase.instance.ref();
+  try {
+    await databaseReference.child('notifications').push().set({'message': message});
+    print('Notification sent successfully: $message');
+  } catch (error) {
+    print('Failed to send notification: $error');
+  }
+}
+
+
 class _PantallaApagadoState extends State<PantallaApagado> {
   void _encender() async {
     try {
       widget.connection.output.add(Uint8List.fromList("ON\n".codeUnits));
+      
+      sendFCMMessage();
       await widget.connection.output.allSent;
+      await _sendNotification('LED Encendido');
       sendNotification("LED Encendido", "El LED ha sido encendido");
 
+          // Enviar notificación FCM
+    
       // Navegar a la pantalla WhitePage
       Navigator.push(
         context,
